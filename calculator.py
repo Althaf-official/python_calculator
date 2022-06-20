8c2363466f428566bccf24489202070bb71e05bb
althaf <althaf.sharafudheen@outlook.com>
2022-06-20 20:15:37 +0400

    create a window

--- calculator.py
+++ calculator.py
@@ -7,3 +7,11 @@ def add_to_field(sth):
     global feild_text
     #so now we will make the  sth as a string and we will update the feild text
     feild_text=feild_text+str(sth)
+
+#now im going to create a window
+window=tk.Tk()
+window.geometry("300x300")
+field=tk.Text(window,height=2,width=20,font=("Times New Roman",20))
+field.grid(row=1,column=1,columnspan=4)
+window.mainloop()
+
